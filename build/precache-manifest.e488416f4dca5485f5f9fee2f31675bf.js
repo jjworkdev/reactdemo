a0e6a46ec3f7609f4ffcd00935b79b0c946e9b5e
jjworkdev <jyoti.jagu.work@gmail.com>
2019-09-27 14:25:29 -0400

    first commit

---
+++ build/precache-manifest.e488416f4dca5485f5f9fee2f31675bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b3fe4f87ecc633b33e52789572cb4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "060f1728b6e0ccba4052",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dc9fea09b218d169144b",
+    "url": "/static/js/2.c29b8756.chunk.js"
+  },
+  {
+    "revision": "060f1728b6e0ccba4052",
+    "url": "/static/js/main.49366f93.chunk.js"
+  },
+  {
+    "revision": "ef2d10d2253531a96a64",
+    "url": "/static/js/runtime-main.c29147d2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
